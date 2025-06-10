562a2b5846399eabb2ecd5a5419a8ffdaaa97029
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-06-10 15:33:23 +0200

    Beheer: fix build met Respec (#12)
    De nieuwe automatisering checks enforceren dat warnings
    ook opgelost worden.

--- js/config.js
+++ js/config.js
@@ -3,11 +3,13 @@ var respecConfig = {
         "label" : "pdf",
         "uri" : "Logius-Beheermodel.pdf"
       } ],
-  authors: [ { 
-        "mailto" : "bomos@logius.nl",
-        "name" : "Logius",
+  authors: [
+    { 
+        "company" : "Logius",
+        "name" : "Edwin Wisse",
         "url" : "https://logius.nl/standaarden"
-      } ],
+    }
+  ],
   editors: [ { 
         "company" : "Logius",
         "name" : "Edwin Wisse",
@@ -16,11 +18,10 @@ var respecConfig = {
   github: "https://github.com/Logius-standaarden/Logius-Beheermodel",
   nl_markdownCodeClass: "code",
   nl_markdownTableClass: "dkkvs",
-  previousPublishDate: "2020-12-31",
-  previousPublishVersion: "0.1",
   pubDomain: "logius",
   publishDate: "2023-09-23",
   publishVersion: "1.0",
+  prevVersion: [],
   shortName: "beheermodel",
   specStatus: "WV",
   specType: "HR"
