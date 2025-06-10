9d727c0c39ba3b9eead631cdcea3f3dfd871349e
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-06-10 10:36:02 +0200

    Zet prevVersion uit

--- js/config.js
+++ js/config.js
@@ -21,6 +21,7 @@ var respecConfig = {
   pubDomain: "logius",
   publishDate: "2023-09-23",
   publishVersion: "1.0",
+  prevVersion: [],
   shortName: "beheermodel",
   specStatus: "WV",
   specType: "HR"
