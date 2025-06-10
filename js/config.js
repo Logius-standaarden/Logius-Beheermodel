952640f199664f42136df74e914e320f863d6d42
Tim van der Lippe <TimvdLippe@users.noreply.github.com>
2025-06-10 10:04:20 +0200

    Fix vorige versie die niet bestaat

--- js/config.js
+++ js/config.js
@@ -18,8 +18,6 @@ var respecConfig = {
   github: "https://github.com/Logius-standaarden/Logius-Beheermodel",
   nl_markdownCodeClass: "code",
   nl_markdownTableClass: "dkkvs",
-  previousPublishDate: "2020-12-31",
-  previousPublishVersion: "0.1",
   pubDomain: "logius",
   publishDate: "2023-09-23",
   publishVersion: "1.0",
